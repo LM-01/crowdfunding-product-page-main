373aa6d6dba0ec67250b9cbe180afb487c3ed993
LM-01 <mollinedo.lester@gmail.com>
2021-03-07 19:37:09 -0500

    auto selection of product

--- crowdfunding-product-page-main/src/components/smallComps/Product.jsx
+++ crowdfunding-product-page-main/src/components/smallComps/Product.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { openModal } from '../../reducers/modalReducer';
+import { selectProduct } from '../../reducers/productReducer';
 
 export function Product(props){
+    const dispatch = useDispatch();
+
     const {heading, pledgeAmount, description, amountLeft} = props.props
     return <>
         <div className="products_container">
@@ -13,7 +18,10 @@ export function Product(props){
         </div>
         <div className="products_container_row">
             <h2 className="products_details--bold">{amountLeft} <span className="products_details--small">left</span></h2>
-            <button className="primary-btn">Select Reward</button>
+            <button className="primary-btn" onClick={()=> {
+                dispatch(selectProduct(heading));
+                dispatch(openModal('backProject'))
+            }}>Select Reward</button>
         </div>
     </div>
     </>
